3ad51b1e8f1f42e697b37d0e468c8639c7570911
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2024-09-02 12:50:30 -0500

    added more text to file

--- write.py
+++ write.py
@@ -1,2 +1,5 @@
 with open('poem.txt', 'w') as file:
     file.write('roses are red,\n')
+    file.write('violets are blue,\n')
+    file.write('sugar is sweet,\n')
+    file.write('and so are you,\n')
