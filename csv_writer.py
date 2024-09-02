784a0ce1be4bcdfe3d62451a7ee346aac7781c07
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2024-09-02 18:02:53 -0500

    created new csv file and wrote data to it

--- csv_writer.py
+++ csv_writer.py
@@ -4,3 +4,4 @@ with open('expensive_pets.csv', 'w') as file:
     csv_writer = csv.writer(file)
 
     csv_writer.writerow(['name', 'species', 'favorite_snack', 'monthly_cost'])
+    csv_writer.writerow(['Buddy', 'dog', 'sliders', 1000])
