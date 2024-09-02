4294437d00d72c4c352451b2c1b3f962f02e3eea
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2024-09-02 17:12:19 -0500

    added next builtin function

--- csv_reader.py
+++ csv_reader.py
@@ -3,6 +3,8 @@ import csv
 with open('dad_jokes.csv', 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
 
+    next(csv_reader)
+
     for row in csv_reader:
         joke = row[1]
         rating = row[2]
