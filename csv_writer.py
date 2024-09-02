60d9e92f76ec96ca068b4e7c656ccbeffea26535
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2024-09-02 18:35:26 -0500

    added data with writerows function

--- csv_writer.py
+++ csv_writer.py
@@ -5,3 +5,9 @@ with open('expensive_pets.csv', 'w', newline='') as file:
 
     csv_writer.writerow(['name', 'species', 'favorite_snack', 'monthly_cost'])
     csv_writer.writerow(['Buddy', 'dog', 'sliders', 1000])
+
+    csv_writer.writerows([
+        ['Daisy', 'dog', 'jerky', 500],
+        ['Rex', 'dog', 'bones', 300],
+        ['Pepper', 'dog', 'cheese', 100]
+    ])
