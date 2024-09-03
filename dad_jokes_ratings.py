f9e86a9a479d472e876e1bffd7643dba5e65dea8
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2024-09-03 10:48:58 -0500

    added context manager and list

--- dad_jokes_ratings.py
+++ dad_jokes_ratings.py
@@ -9,3 +9,14 @@ def rating_category(rating):
         category = 'bad'
     
     return category
+
+modified_dad_jokes = []
+
+with open('dad_jokes.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+
+    headers = next(csv_reader)
+    headers.append('rating_category')
+
+    for row in csv_reader:
+        rating_category(row[2])
