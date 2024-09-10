f24aea0b3292207b45687aea88701abb06f229b0
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2024-09-10 10:04:42 -0500

    used exists method w/control flow to test and handle error

--- paths_and_folders/other_paths.py
+++ paths_and_folders/other_paths.py
@@ -1,3 +1,11 @@
 from pathlib import Path
 
 other_path = Path.home() / 'I' / 'dont' / 'exist.csv'
+
+print(other_path)
+
+if other_path.exists():
+    with open(other_path, 'r') as file:
+        print(file.read())
+else:
+    print('The file does not exist.')
